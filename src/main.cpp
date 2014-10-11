1fa706f576bc5a9c003bcf4040424216e0506fa3
Marian Triebe <m.triebe@live.de>
2014-10-11 14:25:32 +0200

    Parse arguments with getopt

--- src/main.cpp
+++ src/main.cpp
@@ -20,78 +20,121 @@
 
 #include <iostream>
 #include <string>
-#include <cstring>
+#include <cstdlib>
+#include <stdint.h>
+#include <getopt.h>
 
 using namespace rna1;
 
 namespace {
-  const char server_port_arg[]     = "--server-port=";
-  const char cam_server_ip_arg[]   = "--cam-server=";
-  const char cam_server_port_arg[] = "--cam-port=";
-  const char with_client_arg[]     = "--with-client";
+  enum short_options {
+    noserver = (char)1,
+    noclient,
+    serverport,
+    camserver,
+    camport,
+    help
+  };
+
+  const struct option options[] = {
+    { "no-server",   no_argument,       0, static_cast<char>(noserver)   },
+    { "no-client",   no_argument,       0, static_cast<char>(noclient)   },
+    { "server-port", required_argument, 0, static_cast<char>(serverport) },
+    { "cam-server",  required_argument, 0, static_cast<char>(camserver)  },
+    { "cam-port=",   required_argument, 0, static_cast<char>(camport)    },
+    { "help",        no_argument,       0, static_cast<char>(help)       }
+  };
+
+  struct arguments {
+    arguments(uint16_t server_port, std::string cam_server,
+              uint16_t cam_port, bool no_client, bool no_server)
+        : m_server_port(server_port),
+          m_cam_server(cam_server),
+          m_cam_port(cam_port),
+          m_no_client(no_client),
+          m_no_server(no_server) {
+      // nop
+    }
+
+    uint16_t    m_server_port;
+    std::string m_cam_server;
+    uint16_t    m_cam_port;
+    bool        m_no_client;
+    bool        m_no_server;
+   private:
+    arguments();
+  };
 } // namespace <anonymous>
 
-/*struct arguments {
-  uint16_t    m_server_port = "1337";
-  std::string m_cam_server  = "localhost";
-  uint16_t    m_cam_port    = 5000;
-  bool        m_clien   t   = false;
-};*/
-
-/*bool is_substr(const char* ptr, const char* end) {
-  if(::strncmp(ptr, end, strlen(ptr) - 1)) {
-    return true;
-  }
-  return false;
+void print_help() {
+  std::cout << "Usage: rna1 [Options]"                          << std::endl
+            << "Options:"                                       << std::endl
+            << "  --no-server          Start without server"    << std::endl
+            << "  --no-client          Start without client"    << std::endl
+            << "  --server-port=<arg>  Port to listen on"       << std::endl
+            << "  --cam-server=<arg>   IP of camera server"     << std::endl
+            << "  --cam-port=<arg>     Port of camera server"   << std::endl
+            << "  --help               Print this help message" << std::endl;
 }
 
-arguments parse_args(int argc, const char* argv[]) {
-  arguments args;
-  for (char* i = argv; i != argv + argc; ++i) {
-    if (is_substr(server_port_arg, *i)) {
-      int p = std::stoi(*i + ::strlen(cam_server_port_arg));
-      args.m_cam_port = static_cast<uint16_t>(p);
-    } else if (is_substr(cam_server_ip_arg, *i)) {
-      args.m_cam_server.assign(*i + ::strlen(cam_server_ip_arg));
-    } else if (is_substr(cam_server_port_arg, *i)) {
-      int p = std::stoi(*i + ::strlen(cam_server_port_arg));
-      args.m_cam_port = static_cast<uint16_t>(p);
-    } else if (is_substr(with_client_arg, *i)) {
-      args.m_client = true;
+arguments parse_args(int argc, char* argv[]) {
+  arguments args(5001, "127.0.0.1", 5000, false, false);
+  int opt = 0;
+  int idx = 0;
+  while ((opt = getopt_long_only(argc, argv, "", options,
+                                &idx)) != -1 ) {
+    switch (opt) {
+      case noserver: {
+        args.m_no_server = true;
+      } break;
+      case noclient: {
+        args.m_no_client = true;
+      } break;
+      case serverport: {
+        args.m_server_port = ::atoi(optarg);
+      } break;
+      case camserver: {
+        args.m_cam_server = optarg;
+      } break;
+      case camport: {
+        args.m_cam_port = ::atoi(optarg);
+      } break;
+      case help: {
+        print_help();
+        exit(0);
+      } break;
+      default: {
+        // unkown argument
+        std::cerr << "Unkown argument (" << opt << ")" << std::endl;
+        print_help();
+        exit(0);
+      } break;
     }
   }
   return args;
-}*/
-
-void print_help() {
-  std::cout << "Usage: rna1 [Options]"                        << std::endl
-            << "Options:"                                     << std::endl
-            << "  --server-port=<arg>  Port to listen on"     << std::endl
-            << "  --cam-server=<arg>   IP of camera server"   << std::endl
-            << "  --cam-port=<arg>     Port of camera server" << std::endl
-            << "  --with-client        Open an client window" << std::endl;
 }
 
-int main(int argc, const char* argv[]) {
-  //if (!parse_args(argc, argv)) {
-  //  print_help();
-  //  return 1;
-  //}
-  
-  fetch_options fopts("127.0.0.1", 5000);
+int main(int argc, char* argv[]) {
+  // parse arguments
+  arguments args = parse_args(argc, argv);
+  fetch_options fopts(args.m_cam_server, args.m_cam_port);
   fetch_impl fetch;
   fetch.start(&fopts);
-
   client_impl client;
-  client.start();
-
+  if (!args.m_no_client) {
+    client.start();
+  }
   server_impl server;
-  server.start();
-
+  if (!args.m_no_server) {
+    server.start();
+  }
   fetch.join();
-  client.join();
-  server.join();
-  
+  if (!args.m_no_client) {
+    client.join();
+  }
+  if (!args.m_no_server) {
+    server.join();
+  }
   delete ring_buffer::get_instance(); // delete singleton
   return 0;
 }
