1acae401f7bf1ce7ce04d1050e5a766ee989e667
Marian Triebe <m.triebe@live.de>
2015-01-01 22:38:12 +0100

    Fix routing from R2 (172.16.12.1) to 172.16.106.2

--- a3/uml/init.sh
+++ a3/uml/init.sh
@@ -105,6 +105,8 @@ case $name in
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
     route add -net 172.16.11.0/24 gw 172.16.15.1
+    route add -net 172.16.103.0/24 gw 172.16.106.1
+    route add -net 172.16.12.0/24 gw 172.16.106.1
 
     # Loesche alle vorhandenen Firewall-Eintraege
     iptables -F
