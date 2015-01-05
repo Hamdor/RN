c66697275beb573cda8f7407a3dc92d89b860c4b
Marian Triebe <m.triebe@live.de>
2015-01-05 02:43:59 +0100

    Add iptables entries for ICMP This allows to use ping inside of the network and ping servers in the internet. The internet can't ping into our network. Also added default gateways to route to the internet.

--- a3/uml/init.sh
+++ a3/uml/init.sh
@@ -31,22 +31,14 @@ echo ' '>/root/.bash_history
 
 
 ifconfig lo 127.0.0.1/24 up
-
+iptables -F
 
 case $name in
   r1)
     ifconfig eth0 172.16.11.1/24 up
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
-    route add -net 172.16.12.0/24 gw 172.16.11.2
-    route add -net 172.16.15.0/24 gw 172.16.11.2
-    route add -net 172.16.14.0/24 gw 172.16.11.2
-    route add -net 172.16.103.0/24 gw 172.16.11.2
-    route add -net 172.16.106.0/24 gw 172.16.11.2
-
-	# Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
+    route add         default      gw 172.16.11.2
   ;;
   r2)
     ifconfig eth0 172.16.11.2/24 up
@@ -57,13 +49,23 @@ case $name in
     route add -net 172.16.14.0/24 gw 172.16.15.2
     route add -net 172.16.103.0/24 gw 172.16.12.3
     route add -net 172.16.106.0/24 gw 172.16.12.3
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
-    iptables -P INPUT DROP
-    iptables -P OUTPUT DROP
+    route add         default      gw 172.16.12.3
+    ## Default Policy DROP
+    iptables -P INPUT   DROP
+    iptables -P OUTPUT  DROP
     iptables -P FORWARD DROP
+    #################################################################
+    ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
+    ## sind, dann kamen diese nicht aus dem Internet               ##
+    #################################################################
+    ## Ping senden
+    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
+    ## Ping antwort
+    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
   ;;
   r3)
     ifconfig eth0 172.16.12.2/24 up
@@ -74,10 +76,8 @@ case $name in
     route add -net 172.16.15.0/24 gw 172.16.12.1
     route add -net 172.16.103.0/24 gw 172.16.12.3
     route add -net 172.16.14.0/24 gw 172.16.12.3
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
+    route add         default     gw 172.16.12.3
+    ## TODO ALLES DROPPEN BIS AUF SSH, HTTP, FTP, PING
   ;;
   r4)
     ifconfig eth0 172.16.12.3/24 up
@@ -88,13 +88,23 @@ case $name in
     route add -net 172.16.106.0/24 gw 172.16.103.1
     route add -net 172.16.14.0/24 gw 172.16.103.1
     route add -net 172.16.15.0/24 gw 172.16.12.1
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
-    iptables -P INPUT DROP
-    iptables -P OUTPUT DROP
+    route add         default     gw 172.16.103.1
+    ## Default Policy DROP
+    iptables -P INPUT   DROP
+    iptables -P OUTPUT  DROP
     iptables -P FORWARD DROP
+    #################################################################
+    ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
+    ## sind, dann kamen diese nicht aus dem Internet               ##
+    #################################################################
+    ## Ping senden
+    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
+    ## Ping antwort
+    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
   ;;
   r5)
     ifconfig eth0 172.16.103.1/24 up
@@ -106,10 +116,27 @@ case $name in
     route add -net 172.16.11.0/24 gw 172.16.103.2
     route add -net 172.16.14.0/24 gw 172.16.106.2
     route add -net 172.16.15.0/24 gw 172.16.106.2
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
+    ## Default Policy DROP
+    iptables -P INPUT   DROP
+    iptables -P OUTPUT  DROP
+    iptables -P FORWARD DROP
+    #################################################################
+    ## Pings von internen Netzwerken erlauben                      ##
+    ## Eingehende Pings werden geblockt                            ##
+    #################################################################
+    ## TODO FIXME
+    ## Ping senden
+    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
+    ## Ping antwort
+    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
+    ## Filter fuer Internes Netz / Internet Pings
+    iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A FORWARD -i eth1 -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A FORWARD -i eth1 -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A FORWARD -i eth2 -p icmp --icmp-type echo-reply -j ACCEPT
   ;;
   r6)
     ifconfig eth0 172.16.106.2/24 up
@@ -120,36 +147,38 @@ case $name in
     route add -net 172.16.11.0/24 gw 172.16.15.1
     route add -net 172.16.103.0/24 gw 172.16.106.1
     route add -net 172.16.12.0/24 gw 172.16.106.1
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
-    iptables -P INPUT DROP
-    iptables -P OUTPUT DROP
+    route add         default     gw 172.16.106.1
+    ## Default Policy DROP
+    iptables -P INPUT   DROP
+    iptables -P OUTPUT  DROP
     iptables -P FORWARD DROP
+    #################################################################
+    ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
+    ## sind, dann kamen diese nicht aus dem Internet               ##
+    #################################################################
+    ## Ping senden
+    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
+    ## Ping antwort
+    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
+    iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
   ;;
   r7)
     ifconfig eth0 172.16.14.2/24 up
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
-    route add -net 172.16.15.0/24 gw 172.16.14.1
-    route add -net 172.16.11.0/24 gw 172.16.14.1
-    route add -net 172.16.12.0/24 gw 172.16.14.1
-    route add -net 172.16.106.0/24 gw 172.16.14.1
-    route add -net 172.16.103.0/24 gw 172.16.14.1
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
+    route add         default      gw 172.16.14.1
+    ## TODO ALLES DROPPEN?
   ;;
   r8)
+    ## Soll dieser PC einen Rechner im Internet darstellen?
+    ## Dann hier keine weiteren Einstellungen ausser den Gateway
     ifconfig eth0 172.16.207.2/24 up
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
-
-    # Loesche alle vorhandenen Firewall-Eintraege
-    iptables -F
-    # hier die iptables-Befehle eintragen:
+    route add         default      gw 172.16.207.1
   ;;
 esac
 
