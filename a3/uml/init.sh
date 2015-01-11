4664f4b33781f27b7da7cda5d513a01af60e3256
Marian Triebe <m.triebe@live.de>
2015-01-11 02:16:59 +0100

    Add local firewall settings to R1, R3 and R7 Also reorganized the file a bit

--- a3/uml/init.sh
+++ a3/uml/init.sh
@@ -29,9 +29,41 @@ echo  '127.0.0.1 localhost
 
 echo ' '>/root/.bash_history
 
-
 ifconfig lo 127.0.0.1/24 up
 iptables -F
+
+## Default Policy DROP
+iptables -P INPUT   DROP
+iptables -P OUTPUT  DROP
+iptables -P FORWARD DROP
+
+## Ping senden
+iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
+iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
+## Ping antwort
+iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
+iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
+
+#################################################################
+## Verbindungen die schon aufgebaut sind akzeptieren           ##
+#################################################################
+iptables -A INPUT   -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT  -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+#################################################################
+## Ausgehende HTTP/FTP Verbindungen                            ##
+#################################################################
+iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 20 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 21 -j ACCEPT
+iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 80 -j ACCEPT
+
+#################################################################
+## HTTP/FTP Forwarding                                         ##
+#################################################################
+iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 20 -j ACCEPT
+iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 21 -j ACCEPT
+iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 80 -j ACCEPT
 
 case $name in
   r1)
@@ -39,6 +71,11 @@ case $name in
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
     route add         default      gw 172.16.11.2
+    #################################################################
+    ## SSH von R1 sowie R7 auf R3 (DMZ) erlauben                   ##
+    #################################################################
+    iptables -A OUTPUT  -p tcp -s 172.16.11.1 -d 172.16.12.2 --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -p tcp -s 172.16.12.2 -d 172.16.11.1 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
   ;;
   r2)
     ifconfig eth0 172.16.11.2/24 up
@@ -50,22 +87,13 @@ case $name in
     route add -net 172.16.14.0/24  gw 172.16.15.2
     route add -net 172.16.103.0/24 gw 172.16.12.3
     route add -net 172.16.106.0/24 gw 172.16.12.3
-
-    ## Default Policy DROP
-    iptables -P INPUT   DROP
-    iptables -P OUTPUT  DROP
-    iptables -P FORWARD DROP
     #################################################################
     ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
     ## sind, dann kamen diese nicht aus dem Internet               ##
     #################################################################
     ## Ping senden
-    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
     ## Ping antwort
-    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
     #################################################################
     ## SSH von R1 sowie R7 auf R3 (DMZ) erlauben                   ##
@@ -87,6 +115,19 @@ case $name in
     route add -net 172.16.103.0/24 gw 172.16.12.3
     route add -net 172.16.106.0/24 gw 172.16.12.3
     route add         default      gw 172.16.12.3
+    #################################################################
+    ## SSH von R1 sowie R7 auf R3 (DMZ) erlauben                   ##
+    #################################################################
+    ## R1
+    iptables -A INPUT  -p tcp -s 172.16.11.1 --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+    ## R7
+    iptables -A INPUT -p tcp -s 172.16.14.2 --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+    #################################################################
+    ## Eingehende HTTP/FTP Verbindungen                            ##
+    #################################################################
+    iptables -A INPUT -m state --state NEW -p tcp --syn --dport 20 -j ACCEPT
+    iptables -A INPUT -m state --state NEW -p tcp --syn --dport 21 -j ACCEPT
+    iptables -A INPUT -m state --state NEW -p tcp --syn --dport 80 -j ACCEPT
   ;;
   r4)
     ifconfig eth0 172.16.12.3/24 up
@@ -98,21 +139,13 @@ case $name in
     route add -net 172.16.14.0/24  gw 172.16.12.1
     route add -net 172.16.15.0/24  gw 172.16.12.1
     route add -net 172.16.106.0/24 gw 172.16.103.1
-    ## Default Policy DROP
-    iptables -P INPUT   DROP
-    iptables -P OUTPUT  DROP
-    iptables -P FORWARD DROP
     #################################################################
     ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
     ## sind, dann kamen diese nicht aus dem Internet               ##
     #################################################################
     ## Ping senden
-    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
     ## Ping antwort
-    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
   ;;
   r5)
@@ -125,20 +158,10 @@ case $name in
     route add -net 172.16.12.0/24 gw 172.16.103.2
     route add -net 172.16.14.0/24 gw 172.16.106.2
     route add -net 172.16.15.0/24 gw 172.16.106.2
-    ## Default Policy DROP
-    iptables -P INPUT   DROP
-    iptables -P OUTPUT  DROP
-    iptables -P FORWARD DROP
     #################################################################
     ## Pings von internen Netzwerken erlauben                      ##
     ## Eingehende Pings werden geblockt                            ##
     #################################################################
-    ## Ping senden
-    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
-    ## Ping antwort
-    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
     ## Filter fuer Internes Netz / Internet Pings
     iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-reply -j ACCEPT
     iptables -A FORWARD -i eth0 -p icmp --icmp-type echo-request -j ACCEPT
@@ -156,21 +179,13 @@ case $name in
     route add -net 172.16.12.0/24 gw 172.16.15.1
     route add -net 172.16.103.0/24 gw 172.16.106.1
     route add         default      gw 172.16.106.1
-    ## Default Policy DROP
-    iptables -P INPUT   DROP
-    iptables -P OUTPUT  DROP
-    iptables -P FORWARD DROP
     #################################################################
     ## Ping Pakete nicht dropen, wenn die Pakete hier angekommen   ##
     ## sind, dann kamen diese nicht aus dem Internet               ##
     #################################################################
     ## Ping senden
-    iptables -A INPUT   -p icmp --icmp-type echo-request -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-request -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-request -j ACCEPT
     ## Ping antwort
-    iptables -A INPUT   -p icmp --icmp-type echo-reply -j ACCEPT
-    iptables -A OUTPUT  -p icmp --icmp-type echo-reply -j ACCEPT
     iptables -A FORWARD -p icmp --icmp-type echo-reply -j ACCEPT
     #################################################################
     ## SSH von R1 sowie R7 auf R3 (DMZ) erlauben                   ##
@@ -188,6 +203,8 @@ case $name in
     # Routing   |     Zielnetz     |   Gateway
     #-----------+------------------+-------------
     route add         default      gw 172.16.14.1
+    iptables -A OUTPUT -p tcp -s 172.16.14.2 -d 172.16.12.2 --sport 513:65535 --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+    iptables -A INPUT -p tcp -s 172.16.12.2 -d 172.16.14.2 --sport 22 --dport 513:65535 -m state --state ESTABLISHED -j ACCEPT
   ;;
   r8)
     ifconfig eth0 172.16.207.2/24 up
@@ -196,23 +213,3 @@ case $name in
     route add         default      gw 172.16.207.1
   ;;
 esac
-#################################################################
-## Verbindungen die schon aufgebaut sind akzeptieren           ##
-#################################################################
-iptables -A INPUT   -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT  -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
-
-#################################################################
-## Ausgehende HTTP/FTP Verbindungen                            ##
-#################################################################
-iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 20 -j ACCEPT
-iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 21 -j ACCEPT
-iptables -A OUTPUT -m state --state NEW -p tcp --syn --dport 80 -j ACCEPT
-
-#################################################################
-## HTTP/FTP Forwarding                                         ##
-#################################################################
-iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 20 -j ACCEPT
-iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 21 -j ACCEPT
-iptables -A FORWARD -m state --state NEW -p tcp --syn --dport 80 -j ACCEPT
